1844d36631a9a445893fcc6b727875d2918aa297
Mykola Sheredko <sheredko.mykola@gmail.com>
2020-12-30 18:34:16 +0200

    DjangoCSV - v0.1.0 - Added navbar to application - Added fontawesome library for icons - Created messaging system for application - Created login form for new users - Added basic scaling for mobile devices

--- main/views.py
+++ main/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login as login_user
+from django.contrib import messages
+
 
 # Create your views here.
 
 def home(response):
+    if not response.user.is_authenticated:
+        return redirect("login")
+
     return render(response, "main/home.html")
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is None:
+            messages.add_message(request, messages.ERROR, "Incorrect email or password.")
+            return redirect("login")
+        else:
+            messages.add_message(request, messages.SUCCESS, "Successfully logged in.")
+            login_user(request, user)
+            return redirect("home")
+    return render(request, "main/login.html")
