f92b32d51fb1657f2ac3ce0c23f387fda88e0c58
Mykola Sheredko <sheredko.mykola@gmail.com>
2021-01-04 02:44:18 +0200

    DjangoCSV - v0.6.3 - Set PostgresSQL to default for production debugging

--- DjangoCSV/settings.py
+++ DjangoCSV/settings.py
@@ -73,24 +73,24 @@ WSGI_APPLICATION = 'DjangoCSV.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if DEBUG:
+"""if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': env("DATABASE_NAME"),
-            'HOST': env("DATABASE_HOST"),
-            'PORT': 5432,
-            'USER': env("DATABASE_USER"),
-            'PASSWORD': env("DATABASE_PASSWORD"),
-        }
+else:"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env("DATABASE_NAME"),
+        'HOST': env("DATABASE_HOST"),
+        'PORT': 5432,
+        'USER': env("DATABASE_USER"),
+        'PASSWORD': env("DATABASE_PASSWORD"),
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
